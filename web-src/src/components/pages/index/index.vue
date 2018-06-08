c0e7fa9078743175aaea851864695fcdcc66c709
yangqi <yangqi@simuwang.com>
2018-06-08 10:48:56 +0800

    '添加less相关webpack配置，topbar公用组件'

---
+++ web-src/src/components/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <topBar></topBar>
+  </div>
+</template>
+
+<script>
+
+import topBar from '@/components/common/topBar.vue'
+export default {
+   components:{
+     topBar
+   }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
+
